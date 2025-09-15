0b9499e554a319f24f1ea77e391002f0fe45d102
Palamuri Varsha <varshapabc123@gmail.com>
2025-09-15 16:02:04 -0500

    folder structure added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Palamuri Varsha',
+    author_email='varshapabc123@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+)
